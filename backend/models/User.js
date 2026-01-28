5f8f856b9ea6534ba2b15a159fff931a08e58905
Aylinn27 <aylin.calderonn27@gmail.com>
2026-01-28 12:54:30 -0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
+
 const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true } 
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'tecnico'],
+    default: 'tecnico'
+  }
 });
+
 module.exports = mongoose.model('User', UserSchema);
